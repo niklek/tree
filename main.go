feabf364616f451df5d91f7a5693fe533e1bd9c2
niklek <nikitaleksikov@gmail.com>
2020-06-09 15:06:12 +0200

    refactor: move listing a path into a separate func, write into a buffer

--- main.go
+++ main.go
@@ -2,12 +2,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
+
+// Default path when no arguments provided
+const pathDefault = "."
 
 // Prints graphic symbols for an item
 func printBar(last bool) string {
@@ -43,39 +47,26 @@ func printSize(fi os.FileInfo, displaySize bool) string {
 	return fmt.Sprintf(" (%d bytes)", fi.Size())
 }
 
-const pathDefault = "."
+// Lists the path into a buffer of bytes
+func listPath(out *bytes.Buffer, path string, listAll bool, listDir bool, displaySize bool) error {
 
-func main() {
 	var (
-		listAll, listDir, displaySize bool
-		path, fullPath, prefix        string
+		s                Stack
+		fullPath, prefix string
 	)
-	flag.BoolVar(&listAll, "a", false, "List all files")
-	flag.BoolVar(&listDir, "d", false, "List directories only")
-	flag.BoolVar(&displaySize, "s", false, "Print the size for each line")
-	flag.Parse()
 
-	// get path or use default
-	args := flag.Args()
-	path = pathDefault
-	if len(args) > 0 {
-		path = args[0]
-	}
-
-	// Create a stack and push the root item
-	var s Stack
+	// Push the root item into the stack
 	s.Push(&item{path, "", true})
 
 	for s.Size() > 0 {
 		it := s.Pop()
 		if it.path == "" {
-			break
+			return nil
 		}
 		// get FileInfo describing the current item
 		fi, err := os.Stat(it.path)
 		if err != nil {
-			log.Println(err)
-			break
+			return err
 		}
 		// when only directories
 		if skipItem(fi, listAll, listDir) {
@@ -111,6 +102,37 @@ func main() {
 		}
 
 		// print the current item
-		fmt.Printf("%s%s%s%s\n", it.prefix, printBar(it.isLast), fi.Name(), printSize(fi, displaySize))
+		_, err = out.WriteString(fmt.Sprintf("%s%s%s%s\n", it.prefix, printBar(it.isLast), fi.Name(), printSize(fi, displaySize)))
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
+}
+
+func main() {
+	var (
+		listAll, listDir, displaySize bool
+		path                          string
+	)
+	flag.BoolVar(&listAll, "a", false, "List all files")
+	flag.BoolVar(&listDir, "d", false, "List directories only")
+	flag.BoolVar(&displaySize, "s", false, "Print the size for each line")
+	flag.Parse()
+
+	// get path or use default
+	args := flag.Args()
+	path = pathDefault
+	if len(args) > 0 {
+		path = args[0]
+	}
+
+	out := new(bytes.Buffer)
+	err := listPath(out, path, listAll, listDir, displaySize)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Print(out)
 }
