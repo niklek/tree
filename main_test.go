8987f6f18dd4b0dd14d0af0a8b2f5704fe93a359
niklek <nikitaleksikov@gmail.com>
2020-06-12 18:40:04 +0200

    tests: add tests for -d and -s flags

--- main_test.go
+++ main_test.go
@@ -20,6 +20,22 @@ const resultTestdataAll = `└──testdata (192 bytes)
         └──file.txt (14 bytes)
 `
 
+const resultTestdataDirOnly = `└──testdata (192 bytes)
+    ├──pkg (96 bytes)
+    ├──lib (96 bytes)
+    |   └──godoc (192 bytes)
+    |       └──images (96 bytes)
+    └──doc (96 bytes)
+`
+
+const resultTestdataDirOnlyNoSize = `└──testdata
+    ├──pkg
+    ├──lib
+    |   └──godoc
+    |       └──images
+    └──doc
+`
+
 // Test listing testdata directory, including sizes
 func TestTestdataAll(t *testing.T) {
 	out := new(bytes.Buffer)
@@ -30,5 +46,31 @@ func TestTestdataAll(t *testing.T) {
 	result := out.String()
 	if result != resultTestdataAll {
 		t.Errorf("bad result\nexpected:\n%v\ngot:\n%v\n", result, resultTestdataAll)
+	}
+}
+
+// Test listing testdata directory, only directories
+func TestTestdataDirOnly(t *testing.T) {
+	out := new(bytes.Buffer)
+	err := listPath(out, "testdata", true, true, true)
+	if err != nil {
+		t.Errorf("unexpected error:%s", err)
+	}
+	result := out.String()
+	if result != resultTestdataDirOnly {
+		t.Errorf("bad result\nexpected:\n%v\ngot:\n%v\n", result, resultTestdataDirOnly)
+	}
+}
+
+// Test listing testdata directory, only directories, no size
+func TestTestdataDirOnlyNoSize(t *testing.T) {
+	out := new(bytes.Buffer)
+	err := listPath(out, "testdata", true, true, false)
+	if err != nil {
+		t.Errorf("unexpected error:%s", err)
+	}
+	result := out.String()
+	if result != resultTestdataDirOnlyNoSize {
+		t.Errorf("bad result\nexpected:\n%v\ngot:\n%v\n", result, resultTestdataDirOnlyNoSize)
 	}
 }
